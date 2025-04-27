3958b2dda15fbd1cffd77005ebe8d133d963114e
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-04-27 00:33:18 -0300

    Adicionando o Unsubscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,17 +7,25 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = ''
+  subscription: Subscription
 
   constructor(private service: LivroService) { }
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe((retornoAPI) => console.log(retornoAPI), )
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.error(erro),
+      complete: () => console.log('Observable completado')
+    })
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
+  }
 }
 
 
